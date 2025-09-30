3de755e752dd285fe7f2b5b99ea8f86fe10597ad
Thieuthieu De Rune <matthieu.thivard03@gmail.com>
2025-09-30 18:19:22 +0200

    TP1

---
+++ bdd/TP 01 - DDL & DML/script.sql
@@ -0,0 +1,128 @@
+# Partie 1
+
+DROP TABLE IF EXISTS pays;
+CREATE TABLE pays (
+    country_id INT PRIMARY KEY AUTO_INCREMENT,
+    country_name VARCHAR(50),
+    region_id VARCHAR(50)
+);
+
+DROP TABLE IF EXISTS departments;
+CREATE TABLE departments (
+    department_id INT PRIMARY KEY AUTO_INCREMENT,
+    department_name VARCHAR(50) NOT NULL,
+    location_id INT NULL
+)
+
+DROP TABLE IF EXISTS employees;
+CREATE TABLE employees (
+    employee_id INT PRIMARY KEY AUTO_INCREMENT,
+    first_name VARCHAR(50),
+    last_name VARCHAR(50),
+    email VARCHAR(50),
+    phone_number VARCHAR(50),
+    hire_date DATE,
+    job_id INT,
+    salary INT UNSIGNED,
+    commission INT,
+    manager_id INT,
+    department_id INT,
+    FOREIGN KEY (department_id) REFERENCES departments(department_id)
+);
+
+DROP TABLE IF EXISTS job_history;
+CREATE TABLE job_history (
+    emplyee_id INT,
+    start_date DATE,
+    end_date DATE,
+    job_id INT,
+    department_id INT,
+    top8salary INT UNSIGNED,
+    FOREIGN KEY (emplyee_id) REFERENCES employees(employee_id),
+    FOREIGN KEY (department_id) REFERENCES departments(department_id)
+);
+
+DROP SCHEMA IF EXISTS Cinema;
+CREATE SCHEMA Cinema;
+USE Cinema;
+
+DROP TABLE IF EXISTS realisateur;
+CREATE TABLE realisateur (
+    id_realisateur INT PRIMARY KEY AUTO_INCREMENT,
+    nom VARCHAR(12)
+);
+
+DROP TABLE IF EXISTS film;
+CREATE TABLE film (
+    id_film INT PRIMARY KEY AUTO_INCREMENT,
+    titre VARCHAR(50) NOT NULL,
+    genre VARCHAR(50),
+    recette DECIMAL UNSIGNED DEFAULT 0,
+    date_sortie DATE,
+    pays_id INT NOT NULL,
+    nb_entrees INT UNSIGNED,
+    date_heure_saisie DATETIME DEFAULT NOW(),
+    resume VARCHAR(255)
+);
+
+ALTER TABLE film ADD COLUMN id_real INT;
+ALTER TABLE film ADD FOREIGN KEY (id_real) REFERENCES realisateur(id_realisateur);
+
+CREATE VIEW FILM2 AS
+SELECT id_film, titre, annee, genre, resume
+FROM film;
+
+DROP VIEW FILM2;
+
+ALTER TABLE film RENAME filmold;
+
+
+# Partie 2
+
+ALTER TABLE filmold RENAME film;
+
+INSERT INTO film (id_film, titre, date_sortie, genre, resume, nb_entrees, recette)
+VALUES (1, 'Subway', 1985, 'Policier', 'Conte les aventures de la population souterraine dans les couloirs du métro parisien', 45662, 4569392),
+(2, 'Nikita', 1990, 'Drame', 'Nikita condamnée à la prison à perpétuité est contrainte à travailler secrètement pour le gouvernement en tant que agent hautement qualifiée des services secrets.', 95662, 155025);
+
+DELETE FROM realisateur;
+DELETE FROM film;
+
+UPDATE film SET genre = 'Anticipation' WHERE id_film = 1;
+
+SELECT * FROM film;
+SELECT titre FROM film;
+SELECT titre FROM film ORDER BY titre ASC;
+SELECT titre FROM film ORDER BY titre DESC;
+SELECT * FROM film WHERE genre = 'Policier';
+SELECT * FROM film WHERE genre = 'Policier' || genre = 'Comédie';
+SELECT titre, date_sortie FROM film;
+SELECT COUNT(*) AS nb_films FROM film;
+SELECT titre, nb_entrees*3 as benefice FROM film;
+SELECT AVG(recette) FROM film;
+SELECT * FROM film WHERE titre LIKE '%a%';
+
+
+
+CREATE SCHEMA Animalerie;
+USE Animalerie;
+
+DROP TABLE IF EXISTS Animaux;
+CREATE TABLE Animaux (
+    id_animal INT PRIMARY KEY AUTO_INCREMENT,
+    nom VARCHAR(50),
+    propriétaire VARCHAR(50),
+    sexe VARCHAR(50),
+    naissance DATE,
+    mort DATE NULL
+);
+
+LOAD DATA INFILE 'C:/Users/Thieu/Documents/Cours/ESEO2/Semestre 7/BDD/bdd/TP 01 - DDL & DML/Animaux.txt'
+INTO TABLE Animaux
+FIELDS TERMINATED BY ',' 
+LINES TERMINATED BY '\n';
+
+INSERT INTO animaux (nom, propriétaire, sexe, naissance) VALUES ('Jojo', 'Diane', 'F', '1999-03-30');
+
+
+# P2
